5d58059f2b6b4c8e93879a9072bce44b291bad31
Yvonne-Ouma <yvonneouma98@gmail.com>
2018-09-03 07:47:21 -0400

    modified my request file

--- app/requests.py
+++ app/requests.py
@@ -1,18 +1,23 @@
-from app import app
 import urllib.request,json
 from .models import source, article
 
 
-Source = source.Source
-Article = article.Article
+# Source = source.Source
+# Article = article.Article
 
 
 # Getting api key
-api_key = app.config['NEWS_API_KEY']
+api_key = None
 
 # Getting the source base url
-base_url = app.config["NEWS_API_BASE_URL"]
-article_url = app.config["ARTICLE_API_BASE_URL"]
+base_url = None
+article_url = None
+
+def configure_request(app):
+    global api_key,base_url,article_url
+    api_key = app.config['NEWS_API_KEY']
+    base_url = app.config["NEWS_API_BASE_URL"]
+    article_url = app.config["ARTICLE_API_BASE_URL"]
 
 def get_sources(category):
     '''
