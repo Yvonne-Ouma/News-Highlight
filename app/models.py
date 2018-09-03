d1b655ed158d3af1e222e5f7131a9e5e8a031710
Yvonne-Ouma <yvonneouma98@gmail.com>
2018-09-03 07:37:56 -0400

    added a class for the sources

--- app/models.py
+++ app/models.py
@@ -1,3 +1,16 @@
+# source class
+class Source:
+    '''
+    Source class to define Objects
+    '''
+
+    def __init__(self,id,name,description):
+        self.id =id
+        self.name = name
+        self.description = description
+
+
+# Article class
 class Article:
     '''
     Article class to define Objects
@@ -12,3 +25,5 @@ class Article:
         self.url = url 
         self.urlToImage = urlToImage
         self.publishedAt = publishedAt
+
+
