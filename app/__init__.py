db8ff13ebc7516a76fed3a95efedbdad66853298
Yvonne-Ouma <yvonneouma98@gmail.com>
2018-09-03 07:32:24 -0400

    changing folder structure

--- app/__init__.py
+++ app/__init__.py
@@ -1,16 +1,17 @@
 from flask import Flask
-from .config import DevConfig
 from flask_bootstrap import Bootstrap
+from config import config_options
 
-# Initializing application
-app = Flask(__name__,instance_relative_config = True)
+bootstrap = Bootstrap()
 
-# Setting up configuration
-app.config.from_object(DevConfig)
-app.config.from_pyfile('config.py')
+def create_app(config_name):
 
-# initializing Flask Extensions
-bootstrap = Bootstrap(app)
+    app = Flask(__name__)
 
-from app import views
-from app import error
+    # Creating the app configurations
+    app.config.from_object(config_options[config_name])
+
+    # initializing Flask Extensions
+    bootstrap.init_app(app)
+
+    return app
