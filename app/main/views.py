657d41cbd04d05963957eff366ede97205f16266
Yvonne-Ouma <yvonneouma98@gmail.com>
2018-09-03 08:06:03 -0400

    modified my views file

--- app/main/views.py
+++ app/main/views.py
@@ -1,9 +1,10 @@
 from flask import render_template
-from app import app
-from .requests import get_sources,get_articles
+from . import main
+from ..requests import get_sources,get_articles
+from ..models import Source,Article
 
 # Views
-@app.route('/')
+@main.route('/')
 def index():
     '''
     View root page function that returns the page and and its data
@@ -19,7 +20,7 @@ def index():
     title = 'Home - Welcome to the NewsHighlights '
     return render_template('index.html',title = title, general = general_source, sports = sports_source,technology =technology_source, entertainment = entertainment_source, business =business_source, science =science_source)
 
-@app.route('/source/<id>')
+@main.route('/source/<id>')
 def articles(id):
     '''
     view source page function that returns the source details page and its data
